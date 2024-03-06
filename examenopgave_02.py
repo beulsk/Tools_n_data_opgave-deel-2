724f0b454d86e0b8f257a50d39af39ea74ec0610
Karb75 <kbeuls@telenet.be>
2024-03-06 22:05:37 +0100

    modified:   examenopgave_02.py

--- examenopgave_02.py
+++ examenopgave_02.py
@@ -1,3 +1,33 @@
+"""
+Opdracht:
+
+Bepalingen:
+ - Je moet gebruik maken van de aangeleverde variable(n)
+ - Je mag deze variable(n) niet aanpassen
+ - Het is de bedoeling dat je op het einde 1 programma hebt
+ - Als inlever formaat wordt een public git url verwacht die gecloned kan worden
+
+/ 5 ptn 1 - Maak een public repository aan op jouw gitlab/github account voor dit project
+/10 ptn 2 - Gebruik python om de gegeven joke_url aan te spreken
+/10 ptn 3 - Gebruik regex om de volgende data te extracten:
+            - De eerste 20 tekens van de joke
+/15 ptn 4 - Verzamel onderstaande data en output alles als yaml. Een voorbeeld vind je hieronder.
+/10 ptn 5 - Doe een post naar de post_url.
+            - Stuur in de post een parameter genaamd "naam" mee met als waarde jouw voornaam
+            - Output het hele antwoord naar de terminal
+            - Haal uit het antwoord van de call jouw ip adres (origin) en geef dit weer met een f string
+
+Totaal  /50ptn
+"""
+
+""" voorbeeld yaml output
+jokes:
+  - intro: <met regex extracted intro>
+    joke: <joke>
+    id: <joke id>
+  - .. <tot aantal jokes bereikt is>
+"""
+
 joke_url = "https://icanhazdadjoke.com"
 amount_of_jokes = 5
 
@@ -34,8 +64,7 @@ for aantal in range(amount_of_jokes):
 # print(joke_gedeelte) 
 # print(" ")
 # print(id_joke)
-    
-        
+            
     output = {
     "intro": twintig_eerste,
     "joke": joke_gedeelte,
@@ -43,8 +72,5 @@ for aantal in range(amount_of_jokes):
     }
 
     totaal.append(output.copy())
-else:
-    print(" ")
-
     
 print(yaml.dump(totaal))
