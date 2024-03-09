49826cc60d76fdcbbd4f6881f59c663224e9aba7
Karb75 <kbeuls@telenet.be>
2024-03-09 20:44:46 +0100

    modified:   requesten_01.py

---
+++ requesten_01.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+import re
+import yaml
+from pprint import pprint
+
+payload = {"name": "Mike", "age": "25"}
+
+# response = requests.post("https://httpbin.org/post", data=payload)
+
+headers = {
+    "User-Agent"
+}
+
+response = requests.get("https://httpbin.org/user-agent")
+# print(response.url)
+
+# if response.status_code ==  requests.codes.not_found:
+#     print("not found")
+# else:
+#     print(response.status_code)
+
+print (response.text)
+
+# res_jason = response.json()
+
+# pprint(res_jason)
