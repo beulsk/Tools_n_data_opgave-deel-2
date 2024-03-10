de95636fbf55b5efc431a6226bc5f9f4d34e0a5e
Karb75 <kbeuls@telenet.be>
2024-03-10 17:38:53 +0100

    modified:   requesten_01.py

--- requesten_01.py
+++ requesten_01.py
@@ -1,1 +1,20 @@
+import requests
+import json
+import re
+import yaml
+from pprint import pprint
+
+
+
+import requests
+
+api_base_url = "https://gitlab.com/api/v4"
+
+response = requests.get(f"{api_base_url}/broadcast_messages")
+
+response_data = response.json()
+pprint(response_data)
+
+
+
 
